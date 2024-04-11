6e5668cc309dfe56f075f4849440e2c79794ea73
123fyq123 <1210915690@qq.com>
2024-04-11 15:48:51 +0800

    start

---
+++ dto/admin_login.go
@@ -0,0 +1,16 @@
+package dto
+
+import (
+	"github.com/e421083458/go_gateway_demo/public"
+	"github.com/gin-gonic/gin"
+)
+
+type AdminLoginInput struct {
+	UserName string `form:"username" json:"username" comment:"用户名"  validate:"required" example:"admin"`
+	Password string `form:"password" json:"password" comment:"密码"  validate:"required" example:"123456"`
+}
+
+// 绑定结构体并校验参数
+func (param *AdminLoginInput) BindValidParam(c *gin.Context) error {
+	return public.DefaultGetValidParams(c, param)
+}
